b3a20230f6a5efb1bf2f76cdebbb253370c93f76
sinan <haxsmith77@gmail.com>
2023-03-25 18:11:33 +1100

    Cleaned up DamageOverlay to properly display damage dealt

--- src/main/java/com/example/DamageOverlay.java
+++ src/main/java/com/example/DamageOverlay.java
@@ -1,22 +1,32 @@
 package com.example;
 
-import net.runelite.api.Client;
 import net.runelite.client.ui.overlay.Overlay;
 import net.runelite.client.ui.overlay.OverlayLayer;
 import net.runelite.client.ui.overlay.OverlayPosition;
 
 import java.awt.*;
+import java.text.DecimalFormat;
 
 public class DamageOverlay extends Overlay {
+
+    private int totalDamage;
+
     public DamageOverlay() {
         setPosition(OverlayPosition.BOTTOM_LEFT);
         setLayer(OverlayLayer.ABOVE_WIDGETS);
     }
 
+    public void updateDamage(int totalDamage) {
+        this.totalDamage = totalDamage;
+    }
+
+
     @Override
     public Dimension render(Graphics2D graphics) {
-        graphics.drawString("TESTESTTESTT", 20, 20);
+        DecimalFormat df = new DecimalFormat("#,###");
+        String damageString = "Total damage dealt: " + df.format(totalDamage);
+        graphics.setColor(Color.WHITE);
+        graphics.drawString(damageString, 20, 20);
         return null;
     }
 }
-
